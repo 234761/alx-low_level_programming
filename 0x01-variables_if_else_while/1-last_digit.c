6f69872af4c57dd3cb4d54caaa9c7a0f4147f78b
234761 <dag234761@gmail.com>
2023-05-16 23:42:04 -0700

    1 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * main - main block
- * This program will assign a random number to the variable n
+ * Discription: Print statement based on the last digit of the random number.
  * and is greater than 5, is 0 and is less than 6 and not 0
  * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
