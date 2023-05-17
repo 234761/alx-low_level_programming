adff290657ae5fc523f4fd0b9e2815f0794bb01f
234761 <dag234761@gmail.com>
2023-05-17 02:27:58 -0700

    4 task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Discription: print all single digit numbers of base 10
+ * strating from 0
+ * Return 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{  
+		i++;
+	}
+	printf("%i \n", i);
+
+	return (0);
+}
