6cb3723134d42d49f0b4f28169e671154b089285
234761 <dag234761@gmail.com>
2023-05-18 05:58:50 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef _holberton_h_
-#define _holberton_h_
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,4 +13,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+
