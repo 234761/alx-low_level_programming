73ae5286df986124562b47a84de41995cea1fe29
234761 <dag234761@gmail.com>
2023-05-19 04:19:16 -0700

    10 task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i < 100)
+	{
+	j = i / 10;
+	k = i / 10;
+	if (j == k)
+	{
+	putchar(i + '0');
+	putchar(i + '0');
+	}
+	if (i < 89)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	i++;
+	}
+	putchar('\n');
+
+	return (0);
+}	
