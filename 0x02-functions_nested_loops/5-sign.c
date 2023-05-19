a58ccff323f808bb83f8211b2713d7aea09fb4f9
234761 <dag234761@gmail.com>
2023-05-19 07:26:16 -0700

    5 task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
 int print_sign(int n);
 {
 	if (n > 0)
-		printf("%d is greater than zero\n", n);
+		_putchar('+');
 	return (1);
 	else if (n < 0)
-		printf("%d is less than zero\n", n);
+		_putchar('-');
 	return (-1);
 	else 
-		printf("% is zero\n", n);
+		_putchar('0');
 	return (0);
 }
