b1e2fb5bf5a3004f521059f0f844f6d166983ea8
234761 <dag234761@gmail.com>
2023-05-19 08:08:51 -0700

    7 task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: 0
+ */
+int print_last_digit(int)
+{
+	int nv;
+
+	if (n > 0) 
+	{
+	nv = -1 * (nv % 10)
+	_putchar(nv + '0');
+	return (nv);
+	}
+	else 
+	nv = (n % 10)
+	_putchar(nv + '0');
+	return (nv);
+}
