b714d194119e595ba80709e6796680b39a2775c3
234761 <dag234761@gmail.com>
2023-05-19 03:38:45 -0700

    8 task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,16 @@
  */
 int main(void)
 {
-	char c;
 	int i;
+	char c;
 
-	for (i = 0; i < 10; i++);
-	{
-		putchar(i + '0');
+	for (i = 0; i < 10; i++)
+		{
+	putchar(i + '0');
 	}
-	
-	for (c = 'a'; c < 'g'; c++) 
-	{
-		putchar(c);
+	for (c = 'a'; c < 'g'; c++)
+		{
+	putchar(c);
 	}
 	putchar('\n');
 
