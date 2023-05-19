a73cc567636b4111716061506c52f15b75fc522f
234761 <dag234761@gmail.com>
2023-05-19 01:43:36 -0700

    3 task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,23 +7,19 @@
  */
 int main(void)
 {
-	char c = 'a';
+	char lower_case;
+	char upper_case;
 
-	while (c <= 'z')
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
-		putchar(c);
-		c++;
+	putchar(lower_case);
 	}
 
-	char c = 'A';
-
-	while (c <= 'Z')
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
-		putchar(c);
-		c++;
+	putchar(upper_case);
 	}
 	putchar('\n');
 
 	return (0);
 }
-
