633a0b748c6cc6915517dd7579416ab1599ef9f1
234761 <dag234761@gmail.com>
2024-07-31 05:04:53 -0700

    my fifth commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with a puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like builiding a multilingual puzzle);
+return (0);
+}
+
+ 
