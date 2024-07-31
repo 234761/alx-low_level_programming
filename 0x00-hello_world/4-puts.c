200cf09b37de0ddb8f1e3372d39a45b0858e771c
234761 <dag234761@gmail.com>
2024-07-31 05:43:16 -0700

    my fifth commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
-
+/**
+ * main - is a program that interprates a puts function
+ *
+ * description - prints "programming is like builiding a multilingual puzzle"
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+     puts("Programming is like building a multilingual puzzle);
+     return(0);
 }
